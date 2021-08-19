6a4ebfbb7518c254a75844891ebc6cb6733493d6
Tim Smith <tsontario@gmail.com>
2021-08-18 23:32:40 -0400

    Beginning to flesh out integration tests

---
+++ test/integration/client_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require "integration_test_helper"
+
+module KubeclientNext
+  class ClientTest < IntegrationTestCase
+    def test_get_configmap
+    end
+  end
+end
