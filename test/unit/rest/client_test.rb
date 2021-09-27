4425b19b91c0f97af42e40e815823cf1a88ab2b1
Tim Smith <tsontario@gmail.com>
2021-09-27 16:41:01 -0400

    Uncomment rest client tests and fix

--- test/unit/rest/client_test.rb
+++ test/unit/rest/client_test.rb
@@ -19,36 +19,41 @@ module K8y
         end
       end
 
-      # def test_get_path_and_subpath
-      #   with_mock_connection(method: :get, path: "https://1.2.3.4/core/v1/configmaps") do
-      #     client = Client.new(config: @config, context: @config.current_context, path: "core/v1")
-      #     client.get("configmaps", as: :raw)
-      #   end
-      # end
+      def test_get_path_and_subpath
+        path = "configmaps"
+        with_client(host: "https://1.2.3.4/core/v1/") do |client|
+          faraday_expectations(client: client, method: :get, path: File.join(client.host, path))
+          client.get(path, as: :raw)
+        end
+      end
 
-      # def test_get_root_path_and_subpath
-      #   with_mock_connection(method: :get, path: "https://1.2.3.4/subpath") do
-      #     client = Client.new(config: @config, context: @config.current_context)
-      #     client.get("subpath", as: :raw)
-      #   end
-      # end
+      def test_get_root_path_and_subpath
+        path = "subpath"
+        with_client(host: "https://1.2.3.4/") do |client|
+          faraday_expectations(client: client, method: :get, path: File.join(client.host, path))
+          client.get(path, as: :raw)
+        end
+      end
 
-      # def test_get_transparently_handles_slashes_in_path_param
-      #   with_mock_connection(method: :get, path: "https://1.2.3.4/path/subpath") do
-      #     client = Client.new(config: @config, context: @config.current_context, path: "/path/")
-      #     client.get("subpath", as: :raw)
-      #   end
-      # end
+      def test_get_transparently_handles_slashes_in_path_param
+        path = "/sub/path/"
+        with_client(host: "https://1.2.3.4/") do |client|
+          faraday_expectations(client: client, method: :get, path: "https://1.2.3.4/sub/path/")
+          client.get(path, as: :raw)
+        end
+      end
 
-      # def test_post_path
-      #   path = "https://1.2.3.4/core/v1/fakeresource"
-      #   data = { fake: "data" }
-      #   headers = { "Content-Type" => "application/json" }
-      #   with_mock_connection(method: :post, path: path, data: data, headers: headers) do
-      #     client = Client.new(config: @config, context: @config.current_context, path: "core/v1")
-      #     client.post("fakeresource", data: data, headers: headers, as: :raw)
-      #   end
-      # end
+      def test_post_path
+        host = "https://1.2.3.4/core/v1/"
+        path = "fakeresource"
+        data = { fake: "data" }
+        headers = { "Content-Type" => "application/json" }
+        with_client(host: host) do |client|
+          faraday_expectations(client: client, method: :post, path: File.join(host, path),
+            data: data, headers: headers)
+          client.post("fakeresource", data: data, headers: headers, as: :raw)
+        end
+      end
 
       def test_put_path
         path = "fakeresource"
@@ -60,39 +65,45 @@ module K8y
         end
       end
 
-      # def test_patch_path_json_patch
-      #   path = "https://1.2.3.4/core/v1/fakeresource"
-      #   data = [{ "op" => "add", "path" => "/fake/path", "value" => "test" }]
-      #   headers = { "Content-Type" => "application/json-patch+json" }
-      #   with_mock_connection(method: :patch, path: path, data: data, headers: headers) do
-      #     client = Client.new(config: @config, context: @config.current_context, path: "core/v1")
-      #     client.patch("fakeresource", strategy: :json, data: data, headers: headers, as: :raw)
-      #   end
-      # end
+      def test_patch_path_json_patch
+        host = "https://1.2.3.4/core/v1/"
+        path = "fakeresource"
+        data = [{ "op" => "add", "path" => "/fake/path", "value" => "test" }]
+        headers = { "Content-Type" => "application/json-patch+json" }
+        with_client(host: host) do |client|
+          faraday_expectations(client: client, method: :patch, path: File.join(host, path),
+            data: data, headers: headers)
+          client.patch("fakeresource", strategy: :json, data: data, headers: headers, as: :raw)
+        end
+      end
 
-      # def test_delete_path
-      #   with_mock_connection(method: :delete, path: "https://1.2.3.4/core/v1/fakeresource") do
-      #     client = Client.new(config: @config, context: @config.current_context, path: "core/v1")
-      #     client.delete("fakeresource", as: :raw)
-      #   end
-      # end
+      def test_delete_path
+        host = "https://1.2.3.4/core/v1/"
+        path = "fakeresource"
+        with_client(host: host) do |client|
+          faraday_expectations(client: client, method: :delete, path: File.join(host, path))
+          client.delete("fakeresource", as: :raw)
+        end
+      end
 
-      # def test_rest_method_returns_error_when_invalid_response_type_set
-      #   with_mock_connection(method: :get, path: "https://1.2.3.4") do
-      #     client = Client.new(config: @config, context: @config.current_context)
-      #     assert_raises(ResponseFormatter::UnsupportedResponseTypeError) { client.get(as: :bogus_type) }
-      #   end
-      # end
+      def test_rest_method_returns_error_when_invalid_response_type_set
+        host = "https://1.2.3.4/"
+        with_client(host: host) do |client|
+          faraday_expectations(client: client, method: :get, path: host)
+          assert_raises(ResponseFormatter::UnsupportedResponseTypeError) { client.get(as: :bogus_type) }
+        end
+      end
 
-      # def test_rest_method_returns_recursive_open_struct_object_by_default
-      #   mock_response = mock_faraday_response
-      #   mock_response.expects(:body).returns(JSON.dump({ fake: "data" }))
-      #   with_mock_connection(method: :get, path: "https://1.2.3.4", returns: mock_response) do
-      #     client = Client.new(config: @config, context: @config.current_context)
-      #     response = client.get
-      #     assert(response.is_a?(RecursiveOpenStruct))
-      #   end
-      # end
+      def test_rest_method_returns_recursive_open_struct_object_by_default
+        host = "https://1.2.3.4/"
+        mock_response = mock_faraday_response
+        mock_response.expects(:body).returns(JSON.dump({ fake: "data" }))
+        with_client(host: host) do |client|
+          faraday_expectations(client: client, method: :get, path: host, returns: mock_response)
+          response = client.get
+          assert(response.is_a?(RecursiveOpenStruct))
+        end
+      end
 
       private
 
